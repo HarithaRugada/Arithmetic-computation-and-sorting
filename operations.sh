7a56c62e762a3b387fb0b22a386a57b60aa956c1
HarithaRugada <haritharugada@gmail.com>
2020-05-02 15:16:05 +0530

    added usecase8

--- operations.sh
+++ operations.sh
@@ -12,3 +12,9 @@ array[2]=$(( $a*$b+$c ))
 array[3]=$(( $c+$a/$b ))
 array[4]=$(( $a%$b+$c ))
 values=( ${array[1]} ${array[2]} ${array[3]} ${array[4]} )
+
+echo "The result of the expression in descending order is"
+for number in ${values[@]}
+do
+	echo $number
+done | sort -nr
